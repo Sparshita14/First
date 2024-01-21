00473ad824d2efe75080c4d901ef892ef1294f5a
Himaja Geetha <himaja_geetha@epam.com>
2024-01-21 21:34:09 +0530

    hello from geetha

---
+++ team-2/geetha/hello from geetha/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class A
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from geetha");
+    }
+}
